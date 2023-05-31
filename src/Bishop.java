6ea2280d1aa11d18f78b811bd328c9a6212d2cfb
lstoeger9269 <lstoeger9269@student.pps.net>
2023-05-31 13:49:37 -0700

    6th

--- src/Bishop.java
+++ src/Bishop.java
@@ -3,4 +3,33 @@ public class Bishop extends Piece{
     public Bishop(boolean taken, boolean isWhite){
         super(taken,isWhite);
     }
+    public boolean canMove(Place start, Place end){
+        if (end.getPiece().getWhite() == this.getWhite()){
+            return false;
+        }
+        int x = Math.abs(start.getX() - end.getX());
+        int y = Math.abs(start.getY() - end.getY());
+
+        if (start.getX() >= 7 || start.getX() <= 0 || start.getY() >= 7 || start.getY() <= 0){
+            return false;
+        }
+
+        if (x != y){
+            return false;
+        }
+
+
+
+    }
+
+    public boolean pieceInWayDiagonal(Place start, Place end){
+
+        int directionX = start.getX() > end.getX() ? 1 : -1;
+        int directionY = start.getY() > end.getY() ? 1 : -1;
+
+        for (int i = 0; i < Math.abs(start.getX() - end.getX())-1; ++i) {
+            if(start.getX()+i*directionX)
+
+        }
+    }
 }
